6b7e9713163ff67bb6e449ae01ac5490b0416181
Lucas Gallone <lucasgallone@outlook.fr>
2025-11-30 21:09:48 +0100

    Update importLogic.ts

--- utils/importLogic.ts
+++ utils/importLogic.ts
@@ -1,6 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
 import { EnsembleInfo, ServiceInfo, GlobalSettings, AudioType, ProtectionLevel } from '../types';
-import { COUNTRIES, LANGUAGES } from '../constants';
 
 // Helper to extract content within a balanced brace block
 const extractBlockContent = (text: string, blockName: string): string | null => {
